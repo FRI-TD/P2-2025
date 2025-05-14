a35103283d745bdc7606d176bdf4038751f8de4c
TD-FRI <td.fri@gmail.com>
2025-05-14 15:15:37 +0200

    p10

---
+++ src/predavanja10/Obmocje.java
@@ -0,0 +1,15 @@
+package predavanja10;
+
+public class Obmocje {
+  double x;
+  double y;
+  double sirina;
+  double visina;
+
+  public Obmocje(double x, double y, double sirina, double visina) {
+    this.x = x;
+    this.y = y;
+    this.sirina = sirina;
+    this.visina = visina;
+  }
+}
